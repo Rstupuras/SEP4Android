4400274f0bc8c03ae9512ccdafe720da0626ec9d
Arnas Cicinskas <arnastr@gmail.com>
2019-05-07 11:52:25 +0200

    theme, nice colors
    .

--- app/src/main/java/com/example/letmebreathe/View/SettingsActivity.java
+++ app/src/main/java/com/example/letmebreathe/View/SettingsActivity.java
@@ -12,5 +12,6 @@ public class SettingsActivity extends PreferenceActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
     }
 }
